cf1fd1dafa745b0a52b0938d42a41508b86ee555
carl-johansson <carl.johansson@elev.ga.ntig.se>
2020-03-17 11:29:12 +0100

    logga in
    logga in ruta

--- script.js
+++ script.js
@@ -7,3 +7,12 @@ function togglemeny() {
 }
 
 burger.addEventListener('click', togglemeny)
+
+var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
