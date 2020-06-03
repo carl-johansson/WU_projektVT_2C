13e2a4f6c398de250528f3f1cb92367d5dff47de
carl-johansson <carl.johansson@elev.ga.ntig.se>
2020-06-03 18:45:21 +0200

    Nästan Klar

--- script.js
+++ script.js
@@ -10,9 +10,32 @@ burger.addEventListener('click', togglemeny)
 
 var modal = document.getElementById('id01');
 
-// When the user clicks anywhere outside of the modal, close it
+//När man klickar utanför området stängs den ner
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
+function myFunction() {
+        alert(document.getElementById("myP").style.display);
+}
+
+//function bokatKlart() {
+    //alert("I am an alert box!");
+//}
+
+function promptMe() {
+    document.getElementById("demo").innerHTML = "Hello World";
+}
+
+//burger 2 för mitt konto
+
+// const nav2 = document.querySelector('.navLinks2')
+// const burger2 = document.querySelector('.burger2')
+
+// function togglemeny() {
+//     nav2.classList.toggle('nav2Active')
+//     burger2.classList.toggle('toggle')    
+// }
+
+// burger2.addEventListener('click', togglemeny)
